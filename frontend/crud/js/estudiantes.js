c3d0770d221ebfe0bf130f2082e6881ffc811679
arroyave <juanjosearroyave0401@gmail.com>
2020-02-11 07:34:41 -0500

    archivo Js modificado

--- frontend/crud/js/estudiantes.js
+++ frontend/crud/js/estudiantes.js
@@ -3,23 +3,42 @@ console.log(" ------------------CRUD ESTUDIANTES----------------------------")
 
 
 
-function obtenerValores(params) {
+let estudiantes=[]
+
+
+function obtenerValores() {
     let id = document.getElementById("id").value
-    console.log("Mi Id: ", id)
+    //console.log("Mi Id: ", id)
 
     let nombre = document.getElementById("nombre").value
-    console.log("Nombre: ", nombre)
+    //console.log("Nombre: ", nombre)
 
     let apellido= document.getElementById("apellido").value
-    console.log("Apellido: ", apellido)
+    //console.log("Apellido: ", apellido)
 
     let edad = document.getElementById("edad").value
-    console.log("Edad: ", edad)
+    //console.log("Edad: ", edad)
 
     let carrera = document.getElementById("carrera").value
-    console.log("Carrera: ", carrera)
+    //console.log("Carrera: ", carrera)
 
     let correo = document.getElementById("correo").value
-    console.log("Correo: ", correo)
+    //console.log("Correo: ", correo)
 
+    //PRIMERA FORMA
+    //let miEstudiante = {}
+    //miEstudiante.id = id
+    //miEstudiante.nombre = nombre
+    //console.log(miEstudiante)
+
+    //SEGUNDA FORMA
+    let miEstudiante = {id,nombre,apellido,edad,carrera,correo}
+    console.log(miEstudiante)
+    return miEstudiante
 }   
+
+function crearEstudiantes() {
+    let estudiante = obtenerValores()
+    estudiantes.push(estudiante)
+    console.log(estudiantes)
+}
