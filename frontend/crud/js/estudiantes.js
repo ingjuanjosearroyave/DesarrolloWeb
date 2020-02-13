51ff498a1c7263b7eaaa0518db059bb87477fe46
Juan Jo <juanjosearroyave0401@gmail.com>
2020-02-13 02:46:21 -0500

    tabla con arrays del insert

--- frontend/crud/js/estudiantes.js
+++ frontend/crud/js/estudiantes.js
@@ -4,6 +4,9 @@ console.log(" ------------------CRUD ESTUDIANTES----------------------------")
 
 
 let estudiantes=[]
+let miEstudiante={};
+let jsonArray;
+
 
 
 function obtenerValores() {
@@ -30,15 +33,53 @@ function obtenerValores() {
     //miEstudiante.id = id
     //miEstudiante.nombre = nombre
     //console.log(miEstudiante)
-
     //SEGUNDA FORMA
-    let miEstudiante = {id,nombre,apellido,edad,carrera,correo}
-    console.log(miEstudiante)
-    return miEstudiante
+    miEstudiante = {id,nombre,apellido,edad,carrera,correo}    
+    //console.log(miEstudiante)
+    //return miEstudiante
+    //showArray(jsonArray,idTable);
 }   
 
 function crearEstudiantes() {
     let estudiante = obtenerValores()
     estudiantes.push(estudiante)
-    console.log(estudiantes)
+    //console.log(estudiantes)
 }
+
+/*function getStudent() {
+    return estudiantes;
+}*/
+
+jsonArray = JSON.stringify(estudiantes);
+
+function showArray(jsonArray, idTable) {
+    var array = JSON.parse(jsonArray)
+    var table = document.getElementById(idTable);
+    for (var i = 0; i < array.length; i++) {
+        var row = table.insertRow(i+1);
+        var x = 0; 
+        for (var index in array[i]) {
+            var cell = row.insertRow(x);
+            cell.innerHTML = array[i][index]
+            x++;
+        }
+    }
+    return table;
+}
+
+/*function drawTable() {
+    var list = getStudent();
+    var tbody = document.querySelector('Estudiantes tbody');
+    tbody.innerHTML='';
+    for (var i = 0; i < list.length; i++) {
+        var row = tbody.insertRow(i);
+        var idcell = row.insertCell(0); 
+        var namecell = row.insertCell(1); 
+        var apellidocell = row.insertCell(2); 
+        var edadcell = row.insertCell(3); 
+        var carreracell = row.insertCell(4);
+        var correocell = row.insertCell(5);       
+        idcell.innerHTML = list[i].id
+        tbody.appendChild(row);    
+    }
+}*/
